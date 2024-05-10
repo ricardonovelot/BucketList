a4f84482e34e18644c43577656ed10af99477fb9
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-05-10 14:37:29 -0600

    “MVVM

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -10,10 +10,9 @@ import MapKit
 
 struct ContentView: View {
     
-    @State private var locations = [Location]()
-    
-    //Also using this property to decide if we show the details sheet
-    @State private var selectedPlace: Location?
+    // all the properties of contentview in our viewModel
+    @State private var viewModel = ViewModel()
+  
     
     // Initial Position
     let startPosition = MapCameraPosition.region(MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 56, longitude: -3), span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)))
@@ -24,7 +23,7 @@ struct ContentView: View {
         //Wrapping on MapReader so we get Map Coordinates
         MapReader { proxy in
             Map(initialPosition: startPosition){
-                ForEach(locations) { location in
+                ForEach(viewModel.locations) { location in
                     Annotation(location.name, coordinate:location.coordinate){
                         Image(systemName: "heart.circle")
                             .resizable()
@@ -33,7 +32,7 @@ struct ContentView: View {
                             .clipShape(.circle)
                         // mapview ususally gets confused between selecting a anotations and creating a new one, thats why we use a different gesture (long press)
                             .onLongPressGesture {
-                                selectedPlace = location
+                                viewModel.selectedPlace = location
                             }
                     }
                 }
@@ -41,19 +40,16 @@ struct ContentView: View {
                 .onTapGesture {
                     position in
                     if let coordinate = proxy.convert(position, from: .local){
-                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-                        locations.append(newLocation)
+                        viewModel.addLocation(at: coordinate)
                     }
                 }
             
             // 
-                .sheet(item: $selectedPlace) { place in // first we pass the selected place (it is an optional but swiftui unwrapp it for us)
+                .sheet(item: $viewModel.selectedPlace) { place in // first we pass the selected place (it is an optional but swiftui unwrapp it for us)
                     
                     // in the edit view we find the location we want to edit and we replace it with the modified location
-                    EditView(location: place) { newLocation in
-                        if let index = locations.firstIndex(of: place){
-                            locations[index] = newLocation
-                        }
+                    EditView(location: place) {
+                        viewModel.update(location: $0)
                     }
                 }
         }
--- ContentView-ViewModel.swift
+++ ContentView-ViewModel.swift
@@ -7,9 +7,49 @@
 
 import Foundation
 import MapKit
+import CoreLocation
 
-extension ContentView{
-    @Observable
+extension ContentView{ // we are saing this is the view model for ContentView
+    @Observable // reports back to any swiftui view that's watching
     class ViewModel{
+        
+        private(set) var locations: [Location] // in private(set) we’ve said that reading locations is fine, but only the class itself can write locations
+        var selectedPlace: Location? //in addition, using this property to decide if we show the details sheet
+        let savePath = URL.documentsDirectory.appending(path: "SavedPlaces")
+        
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath) // get data to decode // var = try data(contenstof: location])
+                locations = try JSONDecoder().decode([Location].self, from: data) //decode the data // var = try js.dec(object.self, from data)
+            } catch {
+                locations = [] //if fails return empty
+            }
+        }
+        
+        func addLocation(at point: CLLocationCoordinate2D){
+            let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: point.latitude, longitude: point.longitude)
+            locations.append(newLocation)
+            save()
+        }
+        
+        func update(location: Location){
+            guard let selectedPlace else {return}
+            
+            if let index = locations.firstIndex(of: selectedPlace){
+                locations[index] = location
+            }
+            save()
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations) // try to encode data // var = try json.encode(object)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection]) // try to write data// try data.write(to: loc, options: [])
+            } catch {
+                print("Unable to save data.")
+            }
+        }
+        
     }
 }
