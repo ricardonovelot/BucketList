94bf4b14da2416b3f4200f27d80b701c8a1f5d7b
ricardonovelot <ricardonovelo97.tools@gmail.com>
2024-05-14 15:53:38 -0600

    MVVM

---
+++ BucketList/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  BucketList
+//
+//  Created by Ricardo on 14/05/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
---
+++ EditView-ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditView-ViewModel.swift
+//  BucketList
+//
+//  Created by Ricardo on 14/05/24.
+//
+
+import Foundation
